edbf65960c3194afb76c0368ed4a578e6d73e9bd
Reetik Bhave <reetikbhave123@gmail.com>
2025-06-26 12:42:55 +0000

    conversion operations

---
+++ 1_Basics/2_conversionOperation.js
@@ -0,0 +1,29 @@
+//DataType Conversion
+
+let score = "55";
+
+//provides the type of variable
+console.log(typeof score)
+console.log(score);
+
+//to convert the string into number
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber)
+
+//For boolean
+let loggedIn = 1;
+console.log(loggedIn);
+
+let booleanIsLoggedIn = Boolean(loggedIn);
+console.log(booleanIsLoggedIn);
+
+//example
+let someNumber = 33;
+console.log(typeof someNumber)
+console.log(someNumber)
+
+// converted into string
+let stringNmuber = String(someNumber) 
+console.log(stringNmuber);
+console.log(typeof stringNmuber);
