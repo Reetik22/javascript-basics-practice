1c5e4c80b5753e6455d1b7e5f32e0062ab2851ce
Reetik Bhave <reetikbhave123@gmail.com>
2025-06-27 08:29:08 +0000

    Postfix And Prefix operators

---
+++ 1_Basics/4_Postfix_Prefix.js
@@ -0,0 +1,34 @@
+//Postfix And Prefix
+
+//Prefix Example
+// let x = 5;
+// let y = ++x;
+
+// console.log(x);
+// console.log(y);
+
+
+//Postfix Example
+// let x = 5;
+// let y = x++;
+
+// console.log(x); //6
+// console.log(y); //5
+
+//With Decrement
+let a = 10;
+let b = --a;
+console.log(a);
+console.log(b);
+
+let c = 10;
+let d = c--;
+console.log(c); //9
+console.log(d); //10
+
+//example
+let count = 0;
+console.log(count++) // 0 (then becomes 1)
+console.log(++count) //2 (first becomes 2, then prints)
+console.log(count--) //2 (then becomes 1)
+console.log(--count) //0
