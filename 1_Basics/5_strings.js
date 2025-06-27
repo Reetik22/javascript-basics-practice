b9d1e2e787e0c472714e6ca78b0a92a4a2c07032
Reetik Bhave <reetikbhave123@gmail.com>
2025-06-27 09:05:52 +0000

    String and  methods on string

---
+++ 1_Basics/5_strings.js
@@ -0,0 +1,43 @@
+//Strings
+//Three ways to initialize string
+// let name = "Reetik";
+// let city = 'Nagpur';
+// let msg = `Hello`;
+
+
+//String Concatenation
+// let firstName = "Reetik";
+// let lastName = "Bhave";
+// let age = 22;
+
+// let fullName = firstName + " " +  lastName;
+// console.log(fullName + age);
+
+
+//String Interpolation
+//Using backticks(`) and ${} to insert values
+// let city = "Nagpur";
+// console.log(`My name is ${firstName} and I'm from ${city}`);
+
+
+//String Methods
+//1.length
+console.log("hello".length)
+
+//2.toUpperCase()
+console.log(`hello`.toUpperCase());
+
+//3.toLowerCase()
+console.log("HELLO".toLowerCase());
+
+//4.Trim (removes whitespace)
+console.log("  hello".trim());
+
+//5.Includes (check if string contains a substring)
+console.log("hello".includes('el'));
+
+//6.indexOf (finds position of substring)
+console.log("hello".indexOf('e'));
+
+//7.charAt(index) (returns character at index)
+console.log("hello".charAt(2));
